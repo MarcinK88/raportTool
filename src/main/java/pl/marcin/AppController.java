331080e5656f040fe93ba3c22684015aa082f9c6
Marcin <marcin3216@gmail.com>
2019-07-19 15:35:35 +0200

    in progress

--- src/main/java/pl/marcin/AppController.java
+++ src/main/java/pl/marcin/AppController.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.swing.*;
@@ -174,6 +177,61 @@ public class AppController {
 
         return "redirect:/test";
     }
+
+    @GetMapping("/editTicket/{id}")
+    public String editTicket(@PathVariable long id, Model model) {
+        System.out.println(id);
+
+        Converted toEdit = convertedRepository.findById(id).get();
+        model.addAttribute("ticket", toEdit);
+
+        List<String> ticketOwners = new ArrayList<>();
+        ticketOwners.add("Matthias Hagen");
+        ticketOwners.add("Marcin Kucharczyk");
+        ticketOwners.add("Katarzyna Rytter");
+        ticketOwners.add("Frank Witzer");
+        model.addAttribute("ticketOwners",ticketOwners);
+
+        List<String> ba = new ArrayList<>();
+        ba.add("CO");
+        ba.add("CT");
+        ba.add("ET");
+        ba.add("IS");
+        ba.add("MX");
+        ba.add("SE");
+        model.addAttribute("ba",ba);
+
+        List<String> region = new ArrayList<>();
+        region.add("EMEA");
+        region.add("APAC");
+        region.add("LATAM");
+        region.add("NA");
+        model.addAttribute("region",region);
+
+        List<String> requestType = new ArrayList<>();
+        requestType.add("SSL Certificate");
+        requestType.add("DNS");
+        requestType.add("IP mgmt");
+        requestType.add("Domain mgmt");
+        requestType.add("Other");
+        model.addAttribute("requestType",requestType);
+
+        List<String> status = new ArrayList<>();
+        status.add("In progress");
+        status.add("Closed");
+        model.addAttribute("status",status);
+
+        return "editticket";
+    }
+
+    @PostMapping("/editTicket/{id}")
+    public String editTicket(@ModelAttribute Converted ticket) {
+
+        convertedRepository.save(ticket);
+        return "redirect:/converted";
+    }
+
+
 
 
 }
