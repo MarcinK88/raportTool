2d65189e4e1c30c695235900c75d0324d4b4758a
Marcin <marcin3216@gmail.com>
2019-07-24 15:19:13 +0200

    request per BA

--- src/main/java/pl/marcin/TableWriter.java
+++ src/main/java/pl/marcin/TableWriter.java
@@ -21,6 +21,7 @@ public class TableWriter {
     private List<String> types;
     private XSSFWorkbook wb;
     private XSSFSheet sheet;
+    private List<String> ba;
 
     public TableWriter() {
 
@@ -28,9 +29,11 @@ public class TableWriter {
         this.types = Arrays.asList("Other", "Domain mgmt", "IP mgmt", "SSL Certificate", "DNS");
         this.years = new ArrayList<>();
         this.months = Arrays.asList("january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december");
-        for(int i = 2016; i <= LocalDate.now().getYear();i++) {
+        for(int i = 2016; i <= LocalDate.now().getYear(); i++) {
             years.add(i);
         }
+        this.ba = Arrays.asList("CO", "IS", "MX", "SE", "ET", "CT");
+
 
 
     }
@@ -38,9 +41,9 @@ public class TableWriter {
 
     public void createOpenedPerMonth(ConvertedRepository convertedRepository) {
 
-          int selectedMonthIndex = this.months.indexOf(this.selectedMonth);
+        int selectedMonthIndex = this.months.indexOf(this.selectedMonth);
 
-        //tworzenie excela
+        //tworzenie arkusza
         sheet = wb.createSheet("opened per month");
         //liczba wierszy i kolumn
         final int NUM_OF_ROWS = 6;
@@ -66,7 +69,9 @@ public class TableWriter {
 
         }
 
+        //kolejne rzędy
         for (int rowIndex = 1; rowIndex < NUM_OF_ROWS; rowIndex++) {
+            //tworzenie rzędu
             row = sheet.createRow((short) rowIndex);
             cell = row.createCell(0);
             cell.setCellValue(this.types.get(rowIndex - 1));
@@ -86,7 +91,69 @@ public class TableWriter {
     }
 
     public void createRequestPerBa(ConvertedRepository convertedRepository) {
+
+        int selectedMonthIndex = this.months.indexOf(this.selectedMonth);
+
+        //tworzenie arkusza
         sheet = wb.createSheet("Requests per BA");
+
+        //liczba wierszy i kolumn
+        final int NUM_OF_ROWS =4;
+        final int NUM_OF_COLUMNS = 7;
+
+        Row row;
+        Cell cell;
+
+        //tworzenie pierwszego rzędu
+        row = sheet.createRow(0);
+
+        //uzupełnianie pierwszego rzędu
+        cell = row.createCell(0);
+        int i = 0;
+        for (int colIndex = 1; colIndex < NUM_OF_COLUMNS; colIndex++) {
+            cell = row.createCell((short) colIndex);
+            cell.setCellValue(this.ba.get(colIndex-1));
+        }
+
+        //kolejne rzędy
+        for (int rowIndex = 1; rowIndex < NUM_OF_ROWS; rowIndex++) {
+            //tworzenie rzędu
+            row = sheet.createRow((short) rowIndex);
+            cell = row.createCell(0);
+
+            //jeżeli wybrany styczeń - luty
+            if(selectedMonthIndex-(NUM_OF_ROWS-rowIndex-1) < 0) {
+                cell.setCellValue(this.months.get(this.months.size() -(NUM_OF_ROWS - rowIndex - selectedMonthIndex - 1)));
+            } else {
+                cell.setCellValue(this.months.get(selectedMonthIndex - (NUM_OF_ROWS - rowIndex - 1)));
+            }
+            i=0;
+            for(int colIndex = 1; colIndex < NUM_OF_COLUMNS; colIndex++){
+                cell = row.createCell(colIndex);
+
+                //jeżeli wybrany styczeń - luty
+                if(selectedMonthIndex-(NUM_OF_ROWS-rowIndex-2) <= 0){
+                    System.out.println("wybrany ba: " + this.ba.get(colIndex - 1));
+                    System.out.println("wybrany rok: " + (selectedYear-1));
+                    System.out.println("wybrany miesiąc: " + (this.months.size() - (NUM_OF_ROWS - rowIndex - selectedMonthIndex)));
+                    System.out.println("wpisana wartość: " + convertedRepository.countRequestPerBa(this.ba.get(colIndex - 1), selectedYear - 1, this.months.size() - (NUM_OF_ROWS - rowIndex - selectedMonthIndex)) );
+                    System.out.println("this.months.size = " + this.months.size());
+                    System.out.println("NUM_OF_ROWS: " + NUM_OF_ROWS);
+                    System.out.println("rowIndex: " + rowIndex);
+                    System.out.println("selectedMonthIndex: " + selectedMonthIndex);
+                    System.out.println("NUM_OF_ROWS - rowIndex - selectedMonthIndex - 2 : " + (NUM_OF_ROWS - rowIndex - selectedMonthIndex - 2));
+                    System.out.println("this.months.size() - (NUM_OF_ROWS - rowIndex - selectedMonthIndex - 2): " + (this.months.size() - (NUM_OF_ROWS - rowIndex - selectedMonthIndex - 2)));
+                                                                                                                            //          12-(4-1-1
+                    cell.setCellValue(convertedRepository.countRequestPerBa(this.ba.get(colIndex - 1), selectedYear - 1, this.months.size() - (NUM_OF_ROWS - rowIndex - selectedMonthIndex - 2)));
+                    i++;
+                } else {
+                    cell.setCellValue(convertedRepository.countRequestPerBa(this.ba.get(colIndex - 1), selectedYear, selectedMonthIndex - (NUM_OF_ROWS - rowIndex - 2)));
+                }
+            }
+
+        }
+
+
     }
 
     public void saveToFile() throws IOException {
